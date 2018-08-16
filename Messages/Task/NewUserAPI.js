17717ae2700d8ef55fdf408451b7a321839c8166
Skullyfox <skullyfox.yt@gmail.com>
2018-08-16 17:51:05 +0200

    new member bug fix

--- Messages/Task/NewUserAPI.js
+++ Messages/Task/NewUserAPI.js
@@ -10,16 +10,7 @@ module.exports = {
         username = member.user.username,
         apiUrl = process.env.API_URL;
 
-    axios.get(apiUrl+"/"+username)
-    .then(response => {
-
-      let infos = JSON.parse(JSON.stringify(response.data.data.user[0]));
-      
-      if(infos === undefined){
-        axios.post(apiUrl+"/"+username+"/0")
-        .then(console.log(username+" ajouté à l'API"))
-      }
-
-    })
+    axios.post(apiUrl+"/"+username+"/0")
+    .then(console.log(username+" ajouté à l'API"))
   }
 }
