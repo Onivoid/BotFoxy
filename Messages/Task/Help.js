b7a51f38ec7352f050779d7b8202f14787bd4620
Skullyfox <skullyfox.yt@gmail.com>
2018-09-08 21:31:23 +0200

    Delete fortniteTracker

--- Messages/Task/Help.js
+++ Messages/Task/Help.js
@@ -17,7 +17,6 @@ module.exports = {
                     .addField(":three: **Foxy ProfanityTop [1 - 5] : **","Permet de connaître le Top de 1 à 5 des membres les plus toxique.")
                     .addField(":four: **Foxy PornCategory : **","Permet de connaître les catégories pour la commande suivante.")
                     .addField(":five: **Foxy Pornpics {Catégorie} : **","Envoie une image de type NSFW de la catégorie de votre choix (+50 catégories sur le site http://pornpics.com/).")
-                    .addField(":six: **Foxy FortniteStats {pc | psn | xbl}:{pseudo} : **","Permet **l'instant** de récupérer les stats Général d'un joueur de Fortnite.")
                     .setFooter("©️Skullyfox#0666",client.users.get('138050601702522880').avatarURL)
     );
   }
